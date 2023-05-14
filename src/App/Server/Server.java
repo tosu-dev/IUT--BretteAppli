b1835301f2d5be9e7f6a11f88a50209beadcf46f
tosu! <59777055+tosu-dev@users.noreply.github.com>
2023-05-14 18:37:13 +0200

    start server code

--- src/App/Server/Server.java
+++ src/App/Server/Server.java
@@ -1,13 +1,53 @@
 package App.Server;
 
+import java.io.IOException;
 import java.sql.*;
+import java.net.*;
 
-public class Server {
+public class Server implements Runnable {
+
+    private int port;
+    private ServerSocket socket;
+
+    public Server(int port) throws IOException {
+        this.port = port;
+        this.socket = new ServerSocket(this.port);
+    }
+
+    @Override
+    public void run() {
+		try {
+			while(true) {
+                // ... SERVICES ...
+            }
+		}
+        // TODO : changer Exception
+		catch (Exception e) {
+			try {
+                this.socket.close();
+            }
+            catch (IOException e_) {
+
+            }
+			System.err.println("Pb sur le port d'écoute :" + e);
+		}
+    }
+
+    protected void finalize() throws Throwable {
+		try {
+            this.socket.close();
+        }
+        catch (IOException e) {
+
+        }
+	}
 
     public static void main(String[] args) {
 
         System.out.println("Hello, i'm the server side !");
 
+        // [Théo] : Je sais pas où mettre ce code je le laisse là pour l'instant
+        // TODO : mettre ce code au bon endroit
         try {
             Class<?> cls = Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/bretteapplidb?user=root");
@@ -16,5 +56,6 @@ public class Server {
         }
 
     }
+
 
 }
