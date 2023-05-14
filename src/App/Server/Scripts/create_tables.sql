08a6195470c1a4a5644dd1a327a016ae6eb95d73
tosu! <59777055+tosu-dev@users.noreply.github.com>
2023-05-14 18:21:23 +0200

    sql scripts

---
+++ src/App/Server/Scripts/create_tables.sql
@@ -0,0 +1,47 @@
+CREATE TABLE bretteapplidb.Suscriber (
+    id BIGINT AUTO_INCREMENT,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    birthdate DATE NOT NULL,
+    CONSTRAINT PK_Suscriber PRIMARY KEY (id)
+);
+
+CREATE TABLE bretteapplidb.TypeDocumentState (
+    id INT AUTO_INCREMENT,
+    label VARCHAR(20) NOT NULL,
+    CONSTRAINT PK_TypeDocumentState PRIMARY KEY (id)
+);
+
+CREATE TABLE bretteapplidb.Document (
+    id BIGINT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    state INT NOT NULL,
+    CONSTRAINT PK_Document PRIMARY KEY (id),
+    CONSTRAINT FK_Document_state FOREIGN KEY (state) REFERENCES TypeDocumentState(id)
+);
+
+CREATE TABLE bretteapplidb.TypeCommand (
+    id INT AUTO_INCREMENT,
+    label VARCHAR(20) NOT NULL,
+    CONSTRAINT PK_TypeCommand PRIMARY KEY (id)
+);
+
+CREATE TABLE bretteapplidb.Command (
+    id BIGINT AUTO_INCREMENT,
+    idSuscriber BIGINT NOT NULL,
+    idDocument BIGINT NOT NULL,
+    date Date NOT NULL,
+    type INT NOT NULL,
+    CONSTRAINT PK_Command PRIMARY KEY (id),
+    CONSTRAINT FK_Command_idSuscriber FOREIGN KEY (idSuscriber) REFERENCES Suscriber(id),
+    CONSTRAINT FK_Command_idDocument FOREIGN KEY (idDocument) REFERENCES Document(id),
+    CONSTRAINT FK_Command_type FOREIGN KEY (type) REFERENCES TypeCommand(id),
+    CONSTRAINT U_Command_idSuscriber_idDocument UNIQUE (idSuscriber, idDocument)
+);
+
+CREATE TABLE bretteapplidb.Dvd (
+    id BIGINT,
+    adult boolean,
+    CONSTRAINT PK_Dvd PRIMARY KEY (id),
+    CONSTRAINT FK_Dvd_id FOREIGN KEY (id) REFERENCES Document(id)
+);
---
+++ src/App/Server/Scripts/drop_tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE bretteapplidb.command;
+
+DROP TABLE bretteapplidb.typecommand;
+
+DROP TABLE bretteapplidb.suscriber;
+
+DROP TABLE bretteapplidb.dvd
+
+DROP TABLE bretteapplidb.document;
+
+DROP TABLE bretteapplidb.typedocumentstate;
+
+
+
+
+
+
