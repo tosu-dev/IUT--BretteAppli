a0d092c0cdd0a8ffa0e65c1188c051eb6e1137e3
tosu! <theo.constant42@gmail.com>
2023-06-01 10:43:49 +0200

    [A]

---
+++ src/App/Server/Scripts/v0.05-insert_datas_dvd.sql
@@ -0,0 +1,22 @@
+-- not adult
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (1, 'Le roi lion 1', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (1, false);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (2, 'Bambi', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (2, false);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (3, 'Blanche neige et les 7 nains', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (3, false);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (4, 'Oliver Twitst', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (4, false);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (5, 'Charlie et la chocolaterie', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (5, false);
+-- adult
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (6, 'Euphoria', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (6, true);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (7, 'ça', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (7, true);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (8, 'Vendredi 13', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (8, true);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (9, 'Massacre à la tronçonneuse', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (9, true);
+INSERT INTO bretteapplidb.document (id, title, state) VALUES (10, '50 nuances de Grey', 1);
+INSERT INTO bretteapplidb.dvd (id, adult) VALUES (10, true);
